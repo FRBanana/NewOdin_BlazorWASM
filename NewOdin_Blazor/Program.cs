e1cf46e4f5c9d6c9c462e6cecbfe2ef27a616e21
ASI\irucas <irucas@csyoutsourcing.com>
2024-06-24 12:07:44 +0800

    added-viewmodel-gantt

--- NewOdin_Blazor/Program.cs
+++ NewOdin_Blazor/Program.cs
@@ -1,11 +1,17 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using NewOdin_Blazor;
+using NewOdin_Blazor.ViewModels;
+using System;
+using System.Net.Http;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// Register the GanttViewModel
+builder.Services.AddScoped<GanttViewModel>();
 
 await builder.Build().RunAsync();
---
+++ NewOdin_Blazor/ViewModels/GanttViewModel.cs
@@ -0,0 +1,21 @@
+﻿// ViewModels/GanttViewModel.cs
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace NewOdin_Blazor.ViewModels
+{
+    public class GanttViewModel
+    {
+        private readonly IJSRuntime _jsRuntime;
+
+        public GanttViewModel(IJSRuntime jsRuntime)
+        {
+            _jsRuntime = jsRuntime;
+        }
+
+        public async Task InitializeGanttAsync()
+        {
+            await _jsRuntime.InvokeVoidAsync("initGantt");
+        }
+    }
+}
