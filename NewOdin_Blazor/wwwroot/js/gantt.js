08d37aae6c1fd427d9ee5050018c42414791d9d3
ASI\irucas <irucas@csyoutsourcing.com>
2024-06-24 11:17:05 +0800

    show-gantt-chart

--- NewOdin_Blazor/wwwroot/js/gantt.js
+++ NewOdin_Blazor/wwwroot/js/gantt.js
@@ -1,4 +1,6 @@
-﻿function initGantt() {
+﻿import * as templates from "./templates.js";
+
+export function initGantt() {
     console.log("hello")
 
     const data = {
@@ -19,6 +21,62 @@
         ]
     }
 
+    initGanttLayout(gantt);
+
     gantt.init("gantt_here");
     gantt.parse(data);
 }
+
+function initGanttLayout(gantt) {
+	gantt.config.columns = templates.defaultColumn(gantt);
+	gantt.config.resource_property = "nice";
+	gantt.config.resource_attribute = "nice-id";
+	//gantt.config.resource_render_empty_cells = true;
+
+	const custom_layout = {
+		css: "gantt_container",
+		rows: [
+			{
+				// Main Chart Config
+				cols: [
+					{ view: "grid", group: "grids", scrollY: "scrollVer" },
+					{ resizer: true, width: 1 },
+					{ view: "timeline", scrollX: "scrollHor", scrollY: "scrollVer" },
+					{ view: "scrollbar", id: "scrollVer", group: "vertical" }
+				],
+				gravity: 1
+			},
+			{ resizer: true, width: 1 },
+			// Resource Load Diagram
+			{
+				config: templates.resourceLoadChart(gantt),
+				cols: [
+					{ view: "resourceGrid", group: "grids", width: 435, scrollY: "resourceVScroll" },
+					{ resizer: true, width: 1 },
+					{ view: "resourceTimeline", scrollX: "scrollHor", scrollY: "resourceVScroll" },
+					{ view: "scrollbar", id: "resourceVScroll", group: "vertical" }
+				],
+				gravity: 1
+			},
+			{ resizer: true, width: 1 },
+			// Resource Capacity Diagram
+			{
+				config: templates.resourceCapacityChart(gantt),
+				cols: [
+					{ view: "resourceGrid", group: "grids", width: 435, scrollY: "resourceVScroll" },
+					{ resizer: true, width: 1 },
+					{ view: "resourceHistogram", scrollX: "scrollHor", scrollY: "resourceVScroll" },
+					{ view: "scrollbar", id: "resourceVScroll", group: "vertical" }
+				],
+				gravity: 1
+			},
+			{ view: "scrollbar", id: "scrollHor" }
+		]
+	};
+
+	// Layout
+	gantt.config.layout = custom_layout;
+}
+
+// Attach functions to the window object to make them globally accessible
+window.initGantt = initGantt;
