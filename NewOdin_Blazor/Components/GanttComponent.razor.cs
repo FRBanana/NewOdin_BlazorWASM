d9e1a84459732b283706181435db4102442cf934
ASI\irucas <irucas@csyoutsourcing.com>
2024-06-27 14:04:27 +0800

    code-moved-behind-razor

---
+++ NewOdin_Blazor/Components/GanttComponent.razor.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using NewOdin_Blazor.Models;
+using System.Threading.Tasks;
+
+namespace NewOdin_Blazor.Components
+{
+    public partial class GanttComponent : ComponentBase
+    {
+        [Inject]
+        protected IJSRuntime JS { get; set; }
+
+        private ElementReference ganttContainer;
+        private IJSObjectReference ganttModule;
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                ganttModule = await JS.InvokeAsync<IJSObjectReference>("import", "./js/component/gantt.js");
+                await InitGanttAsync();
+            }
+        }
+
+        public async Task InitGanttAsync()
+        {
+            if (ganttModule is not null)
+            {
+                await ganttModule.InvokeVoidAsync("initGantt", ganttContainer);
+            }
+        }
+
+        public async Task DisplayDataAsync(GanttDataModel ganttData)
+        {
+            if (ganttModule is not null)
+            {
+                await ganttModule.InvokeVoidAsync("loadGanttData", ganttData);
+            }
+        }
+    }
+}
---
+++ NewOdin_Blazor/Pages/GanttPage.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using NewOdin_Blazor.Components;
+using NewOdin_Blazor.ViewModels;
+using System.Threading.Tasks;
+
+namespace NewOdin_Blazor.Pages
+{
+    public partial class GanttPage : ComponentBase
+    {
+        [Inject]
+        protected GanttViewModel GanttViewModel { get; set; }
+
+        private GanttComponent ganttComponent;
+
+        private async Task DisplayGantt()
+        {
+            var ganttData = await GanttViewModel.GetGanttDataAsync();
+            await ganttComponent.DisplayDataAsync(ganttData);
+        }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+        }
+    }
+}
